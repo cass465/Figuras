d9d80008cf04575cb6dc23739e1fdf329cffc639
YEISON CIFUENTES <yeisoncifuentes2@gmail.com>
2020-02-26 17:41:35 -0500

    Ajuste
    llamado Frame en Main  y eliminacion de codigo innecesario en la clase FrameFigura

--- FigurasGeometricas/src/edu/ucundinamarca/principal/Main.java
+++ FigurasGeometricas/src/edu/ucundinamarca/principal/Main.java
@@ -16,7 +16,11 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here        
-        new FrameFirura().setVisible(true);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrameFirura().setVisible(true);
+            }
+        });
         
     }
     
